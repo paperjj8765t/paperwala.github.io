2d24515524bc755dec0575a61a4ae4d30ee3c9c8
paperwala <129311040+paperwala@users.noreply.github.com>
2023-12-21 17:01:37 +0530

    Add files via upload

---
+++ script/main.js
@@ -0,0 +1,14 @@
+function imgClicked(obj) {
+    var modal = document.getElementById("myModal");
+    var modalImg = document.getElementById("img01");
+    var captionText = document.getElementById("caption");
+
+    modal.style.display = "block";
+    modalImg.src = obj.src;
+    captionText.innerHTML = obj.title;
+}
+
+function closeModal() {
+    var modal = document.getElementById("myModal");
+    modal.style.display = "none";
+}
